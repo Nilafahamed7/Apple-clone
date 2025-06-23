63b03c6096ef22d94e963285a168836f787b230b
nilafahamed7 <nilafiphone@gmail.com>
2025-06-23 18:34:23 +0530

    completed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -16,12 +16,12 @@ function Hero() {
 
             <div className="flex overflow-y-hidden scrollbar-hide gap-4 overflow-x-auto mx-6 md:ml-16">
 
-                <div className=" mt-6 bg-white p-4 rounded-2xl flex-shrink-0  transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl" style={{ width: "40%" }}>
-                    <h2 className="font-semibold text-2xl" >Save on Mac and iPad for college, choose AirPods or an eligible accessory.◊◊◊</h2>
+                <div className=" mt-6 bg-white p-4 rounded-2xl flex-shrink-0  transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl w-96" >
+                    <h2 className="font-semibold text-lg md:text-2xl" >Save on Mac and iPad for college, choose AirPods or an eligible accessory.</h2>
                     <img className="md:w-3/4" src={p1}></img>
                 </div>
 
-                <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl" style={{ width: "40%" }}>
+                <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl w-96" >
                     <p className="text-red-500">OFFER ELIGIBLE</p>
                     <h3 className="font-semibold text-2xl">ipad Air</h3>
                     <p style={{ color: "#BF4295" }}>Apple Intelligence∆</p>
@@ -29,7 +29,7 @@ function Hero() {
                     <img className="md:w-3/4"  src={p2}></img>
                 </div>
 
-              <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl" style={{ width: "40%" }}>
+              <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl w-96">
                     <p className="text-red-500">OFFER ELIGIBLE</p>
                     <h3 className="font-semibold text-2xl">MacBook Air</h3>
                     <p style={{ color: "#BF4295" }}>Apple Intelligence∆</p>
@@ -37,7 +37,7 @@ function Hero() {
                     <img className="md:w-3/4" src={p3}></img>
                 </div>
 
-                <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl" style={{ width: "40%" }}>
+                <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl w-96" >
                     <p className="text-red-500">OFFER ELIGIBLE</p>
                     <h3 className="font-semibold text-2xl">MacBook Pro</h3>
                     <p style={{ color: "#BF4295" }}>Apple Intelligence∆</p>
@@ -45,7 +45,7 @@ function Hero() {
                     <img className="md:w-3/4"  src={p4}></img>
                 </div>
 
-                <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl" style={{ width: "40%" }}>
+                <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-2 hover:rotate-y-2 hover:shadow-xl w-96">
                     <p className="text-red-500">OFFER ELIGIBLE</p>
                     <h3 className="font-semibold text-2xl">iPad Pro</h3>
                     <p className="font-semibold">Built for Apple Intelligence.∆</p>
@@ -53,7 +53,7 @@ function Hero() {
                    <img className="md:w-3/4" src={p5}></img>
                 </div>
 
-                 <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-1 hover:rotate-y-1 hover:shadow-xl" style={{ width: "40%" }}>
+                 <div className="bg-white mt-6 p-4 rounded-2xl flex-shrink-0 transition-transform duration-300 transform hover:scale-105 hover:rotate-x-1 hover:rotate-y-1 hover:shadow-xl w-96">
                     <p className="text-red-500">OFFER ELIGIBLE</p>
                     <h3 className="font-semibold text-2xl">iMac</h3>
                     <p style={{ color: "#BF4295" }}>Apple Intelligence∆</p>
